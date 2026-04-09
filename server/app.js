54e90131cf989ef738b820b5ff15388bb0d146c8
simon <simon@yint.com>
2026-04-09 08:17:06 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,95 @@
+import { Hono } from "@hono/hono";
+import { cors } from "@hono/hono/cors";
+import { logger } from "@hono/hono/logger";
+import { cache } from "@hono/hono/cache";
+import { Redis } from "ioredis";
+import postgres from "postgres";
+
+const app = new Hono();
+const sql = postgres();
+const redis = new Redis(6379, "redis");
+
+const redisCacheMiddleware = async (c, next) => {
+  const cachedResponse = await redis.get(c.req.url);
+  if (cachedResponse) {
+    const res = JSON.parse(cachedResponse);
+    return Response.json(res.json, res);
+  }
+
+  await next();
+
+  if (!c.res.ok) {
+    return;
+  }
+
+  const clonedResponse = c.res.clone();
+
+  const res = {
+    status: clonedResponse.status,
+    statusText: clonedResponse.statusText,
+    headers: Object.fromEntries(clonedResponse.headers),
+    json: await clonedResponse.json(),
+  };
+
+  await redis.set(c.req.url, JSON.stringify(res));
+};
+
+app.use("/*", cors());
+app.use("/*", logger());
+
+app.get(
+  "/",
+  cache({
+    cacheName: "hello-cache",
+    wait: true,
+  }),
+);
+
+app.get(
+  "/",
+  async (c) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return c.json({ message: "Hello world!" });
+  },
+);
+
+app.get("/todos", async (c) => {
+  const todos = await sql`SELECT * FROM todos`;
+  return c.json(todos);
+});
+
+app.post(
+  "/", async (c) => {
+    await caches.delete("hello-cache");
+    return c.json({ message: "Cache cleared!" });
+  },
+);
+
+app.get("/redis-test", async (c) => {
+  let count = await redis.get("test");
+  if (!count) {
+    count = 0;
+  } else {
+    count = Number(count);
+  }
+
+  count++;
+
+  await redis.set("test", count);
+  return c.json({ count });
+});
+
+app.get(
+  "/hello/*",
+  redisCacheMiddleware,
+);
+
+app.get(
+  "/hello/:name",
+  async (c) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return c.json({ message: `Hello ${c.req.param("name")}!` });
+  },
+);
+
+export default app;
