298ae1f7bd7451d334ebb961386c942fde0332b3
simon <simon@yint.dev>
2026-04-20 14:27:36 +0200

    csc/state-management
    Signed-off-by: simon <simon@yint.dev>

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import deno from "@deno/astro-adapter";
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   adapter: deno(),
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
 });
